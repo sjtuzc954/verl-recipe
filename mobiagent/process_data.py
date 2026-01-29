c4b217e0f6ab663601d9eec801aa65b4e93757e9
Cheng Zhang <2694824893@qq.com>
2026-01-29 12:17:09 +0800

    add sharegpt data format

--- mobiagent/process_data.py
+++ mobiagent/process_data.py
@@ -1,14 +1,46 @@
 import argparse
 from pathlib import Path
-import json
 from PIL import Image
 import datasets
 
-def make_map_fn(split, data_source="mobiagent"):
-    def process_fn(example, idx):
+def make_map_fn(split, format, data_source="mobiagent"):
+    def process_fn_sharegpt(example, idx):
+        messages = example.pop("messages")
+        system_prompt = messages[0]["content"]
+        user_prompt = messages[1]["content"]
+        output = messages[2]["content"]
+        images = example.pop("images")
+        ground_truth = output
+        
+        pil_img = Image.open(images[0]).convert("RGB")
+        images = [pil_img]
+
+        data = {
+            "data_source": data_source,
+            "prompt": [
+                {
+                    "role": "system",
+                    "content": system_prompt
+                },
+                {
+                    "role": "user",
+                    "content": user_prompt,
+                }
+            ],
+            "images": images,
+            "reward_model": {"style": "rule", "ground_truth": ground_truth},
+            "extra_info": {
+                "split": split,
+                "idx": idx,
+            },
+        }
+        return data
+
+    # alpaca format
+    def process_fn_alpaca(example, idx):
         instruction = example.pop("instruction")
         output = example.pop("output")
-        example.pop("input")
+        example.pop("input", None)
         images = example.pop("images")
         ground_truth = output
 
@@ -32,11 +64,12 @@ def make_map_fn(split, data_source="mobiagent"):
         }
         return data
 
-    return process_fn
+    return process_fn_alpaca if format == "alpaca" else process_fn_sharegpt
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--train_file", type=Path, required=True)
+    parser.add_argument("--format", choices=["alpaca", "sharegpt"], default="sharegpt")
     parser.add_argument("--num_samples", type=int, default=None)
     parser.add_argument("--output_dir", type=Path, required=True)
     parser.add_argument("--split_ratio", type=float, default=0.9)
@@ -53,8 +86,7 @@ if __name__ == "__main__":
             seed=4
         )["train"]
 
-    # Use single process to avoid PIL Image serialization issues
-    train_dataset = train_dataset.map(make_map_fn("train"), with_indices=True, num_proc=16)
+    train_dataset = train_dataset.map(make_map_fn("train", args.format), with_indices=True, num_proc=16)
     
     # split train and test
     if args.split_ratio < 1.0:
